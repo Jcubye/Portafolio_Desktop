06033e8efff2fa7a6e35145bcce3b59f3d35af37
JaimeBeldaño <j.beldano@duocuc.cl>
2022-11-30 15:45:08 -0300

    act de dashboard

--- CapaGUI/PantallaAdminDashboard.cs
+++ CapaGUI/PantallaAdminDashboard.cs
@@ -17,16 +17,6 @@ namespace CapaGUI
             InitializeComponent();
         }
 
-        private void perfilToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void salirToolStripMenuItem2_Click(object sender, EventArgs e)
         {
             this.Dispose();
@@ -38,10 +28,49 @@ namespace CapaGUI
             
         }
 
-        private void mantenedorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             MantenedorClientes mc = new MantenedorClientes();
             mc.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 0;
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 1) {
+                CapaNegocio.NegocioCliente auxNeg = new CapaNegocio.NegocioCliente();
+                this.dataGridView1.DataSource = auxNeg.consultaCliente();
+                this.dataGridView1.DataMember = "usuarios";
+
+                if (this.dataGridView1.Rows.Count < 1)
+                {
+                    MessageBox.Show("No hay clientes a listar", "Ups!");
+                    //this.btnActualizar.Enabled = false;
+                }
+                else
+                {
+                    dataGridView1.Columns[0].HeaderText = "Correo";
+                    dataGridView1.Columns[1].HeaderText = "Clave";
+                    dataGridView1.Columns[2].HeaderText = "Rut";
+                    dataGridView1.Columns[3].HeaderText = "Direccion";
+                    dataGridView1.Columns[4].HeaderText = "Telefono";
+                    dataGridView1.Columns[5].HeaderText = "Razon social";
+                    dataGridView1.Columns[6].HeaderText = "Estado";
+                    //this.btnActualizar.Enabled = true;
+                }
+            }
+            if (tabControl1.SelectedIndex == 8)
+            {
+                if ((MessageBox.Show("¿Esta seguro de Salir?", "Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+                {
+                    this.Dispose();
+                    System.GC.Collect();
+                }   
+            }
         }
     }
 }
