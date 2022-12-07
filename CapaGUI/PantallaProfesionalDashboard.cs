88bd7a109b7f78380e93bdd8b03de1ab3a1947d5
CamiloSilvaO <114441372+CamiloSilvaO@users.noreply.github.com>
2022-12-07 09:02:31 -0300

    Add files via upload
    dashboard profesionales

--- CapaGUI/PantallaProfesionalDashboard.cs
+++ CapaGUI/PantallaProfesionalDashboard.cs
@@ -15,6 +15,24 @@ namespace CapaGUI
         public PantallaProfesionalDashboard()
         {
             InitializeComponent();
+
+
+            CapaNegocio.NegocioCliente auxCliente = new CapaNegocio.NegocioCliente();
+            this.dataGridView4.DataSource = auxCliente.consultaClienteActivoCamilo();
+            this.dataGridView4.DataMember = "usuarios";
+
+
+            dataGridView4.Columns[0].HeaderText = "c.id";
+            dataGridView4.Columns[1].HeaderText = "Detalle";
+            dataGridView4.Columns[2].HeaderText = "Fecha Inicio";
+            dataGridView4.Columns[3].HeaderText = "Fecha Termino";
+            dataGridView4.Columns[4].HeaderText = "Cliente ID";
+            dataGridView4.Columns[5].HeaderText = "Razon social";
+            dataGridView4.Columns[6].HeaderText = "Estado";
+            dataGridView4.Columns[7].HeaderText = "Usuario id";
+            dataGridView4.Columns[7].HeaderText = "Rubro id";
+
+
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -26,6 +44,44 @@ namespace CapaGUI
         {
             this.Dispose();
             System.GC.Collect();
+        }
+
+        private void Tabcontrol1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (Tabcontrol1.SelectedIndex == 6)
+            {
+                if ((MessageBox.Show("¿Esta seguro de Salir?", "Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+                {
+                    this.Dispose();
+                    System.GC.Collect();
+                }
+            }
+        }
+
+        private void PantallaProfesionalDashboard_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'prueba_portafolioDataSet.fiscalizacion' Puede moverla o quitarla según sea necesario.
+            this.fiscalizacionTableAdapter.Fill(this.prueba_portafolioDataSet.fiscalizacion);
+            // TODO: esta línea de código carga datos en la tabla 'prueba_portafolioDataSet.solicitud' Puede moverla o quitarla según sea necesario.
+            this.solicitudTableAdapter.Fill(this.prueba_portafolioDataSet.solicitud);
+            // TODO: esta línea de código carga datos en la tabla 'prueba_portafolioDataSet.servicio' Puede moverla o quitarla según sea necesario.
+            this.servicioTableAdapter.Fill(this.prueba_portafolioDataSet.servicio);
+            // TODO: esta línea de código carga datos en la tabla 'prueba_portafolio_profesionalDataSet.profesional' Puede moverla o quitarla según sea necesario.
+           // this.profesionalTableAdapter.Fill(this.prueba_portafolio_profesionalDataSet.profesional);
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+            this.Dispose();
+            System.GC.Collect();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
